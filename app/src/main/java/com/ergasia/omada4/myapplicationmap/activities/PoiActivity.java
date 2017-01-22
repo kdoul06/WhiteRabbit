e915c641ef4e6e908bb3fc0e3547575eac3f57d3
kdoul06 <kdoul06@gmail.com>
2017-01-22 16:52:36 +0200

    kdoul06 commit after pull

--- app/src/main/java/com/ergasia/omada4/myapplicationmap/activities/PoiActivity.java
+++ app/src/main/java/com/ergasia/omada4/myapplicationmap/activities/PoiActivity.java
@@ -1,17 +1,20 @@
 package com.ergasia.omada4.myapplicationmap.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.ergasia.omada4.myapplicationmap.service.GeoService;
-import com.ergasia.omada4.myapplicationmap.entities.Poi;
 import com.ergasia.omada4.myapplicationmap.R;
+import com.ergasia.omada4.myapplicationmap.entities.Poi;
+import com.ergasia.omada4.myapplicationmap.Services.GeoService;
 import com.google.firebase.database.DatabaseReference;
+
 import com.google.firebase.database.FirebaseDatabase;
+
+
 
 public class PoiActivity extends AppCompatActivity {
 
