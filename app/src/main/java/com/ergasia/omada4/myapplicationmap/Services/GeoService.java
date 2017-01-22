3ca8476dbe9c101d9af344fe2d555b323de3c56f
nektarios <nektarios.arakas@gmail.com>
2017-01-22 14:57:33 +0200

    added playground image

---
+++ app/src/main/java/com/ergasia/omada4/myapplicationmap/Services/GeoService.java
@@ -0,0 +1,33 @@
+package com.ergasia.omada4.myapplicationmap.Services;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by nekta on 22/01/2017.
+ */
+
+public class GeoService {
+
+
+    public static String getAddress(Context context, double lat, double lon) {
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        String address = null;
+        try {
+            List<Address> addressList = geocoder.getFromLocation(lat, lon, 1);
+            address = addressList.iterator().next().getAddressLine(0);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return address;
+    }
+
+
+
+}
